6a2facdbdfaea51bf73e8175f69d8dcf36400293
Olvera García Nadia <322074730@pcpuma.acatlan.unam.mx>
2025-09-12 22:42:09 -0600

    Update p_arreglo_cuenta.c

--- data_structure/homework/activities/p_arreglo_cuenta.c
+++ data_structure/homework/activities/p_arreglo_cuenta.c
@@ -2,36 +2,26 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    int pares[10];
-    
-    for (int i = 0; i < 10; i++) 
-        pares[i] = (i + 1) * 2;
-    
-    printf("| ");
-    for (int i = 9; i >= 0; i--) 
-        printf("%d | ", pares[i]);
-    printf("\n");
-    
-    if (argc > 1) {
-        char *cuenta = argv[1];
-        int ultimo = cuenta[strlen(cuenta) - 1] - '0';
-        
-        for (int i = 0; i < 10; i++) {
-            if (pares[i] == ultimo * 2) {
-                pares[i] = -1;
-                break;
-            }
-        }
+    if (argc < 2) {
+        printf("Uso: %s <numero_de_cuenta>\n", argv[0]);
+        return 1;
     }
     
-    printf("Indice | ");
-    for (int i = 0; i < 10; i++) 
-        printf("%d | ", i);
+    int ultimo_digito = argv[1][strlen(argv[1])-1] - '0';
+    int pares[10];
+    
+    for (int i = 0; i < 10; i++) pares[i] = (i+1)*2;
+    
+    printf("| ");
+    for (int i = 9; i >= 0; i--) printf("%d | ", pares[i]);
     printf("\n");
     
-    printf("Valor  | ");
-    for (int i = 0; i < 10; i++) 
-        printf("%d | ", pares[i]);
+    if (ultimo_digito >= 0 && ultimo_digito < 10) pares[ultimo_digito] = -1;
+    
+    printf("Indice | ");
+    for (int i = 0; i < 10; i++) printf("%d | ", i);
+    printf("\nValor  | ");
+    for (int i = 0; i < 10; i++) printf("%d | ", pares[i]);
     printf("\n");
     
     return 0;
