28887afe190fd591c6248a749ebb883c2a747b6d
Olvera García Nadia <322074730@pcpuma.acatlan.unam.mx>
2025-09-12 22:38:31 -0600

    Update p_arreglo_cuenta.c

--- data_structure/homework/activities/p_arreglo_cuenta.c
+++ data_structure/homework/activities/p_arreglo_cuenta.c
@@ -1,44 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Error: Debe proporcionar el número de cuenta como argumento.\n");
-        printf("Uso: %s <numero_de_cuenta>\n", argv[0]);
-        return 1;
-    }
+    int pares[10];
     
-    char *num_cuenta = argv[1];
-    int cantidad_numeros = 10;
-    int longitud = strlen(num_cuenta);
-    int ultimo_digito = num_cuenta[longitud - 1] - '0';
-    
-    int pares[cantidad_numeros];
-    for (int i = 0; i < cantidad_numeros; i++) {
+    for (int i = 0; i < 10; i++) 
         pares[i] = (i + 1) * 2;
-    }
     
     printf("| ");
-    for (int i = cantidad_numeros - 1; i >= 0; i--) {
+    for (int i = 9; i >= 0; i--) 
         printf("%d | ", pares[i]);
-    }
     printf("\n");
     
-    if (ultimo_digito >= 0 && ultimo_digito < cantidad_numeros) {
-        pares[ultimo_digito] = -1;
+    if (argc > 1) {
+        char *cuenta = argv[1];
+        int ultimo = cuenta[strlen(cuenta) - 1] - '0';
+        
+        for (int i = 0; i < 10; i++) {
+            if (pares[i] == ultimo * 2) {
+                pares[i] = -1;
+                break;
+            }
+        }
     }
     
     printf("Indice | ");
-    for (int i = 0; i < cantidad_numeros; i++) {
+    for (int i = 0; i < 10; i++) 
         printf("%d | ", i);
-    }
     printf("\n");
     
     printf("Valor  | ");
-    for (int i = 0; i < cantidad_numeros; i++) {
+    for (int i = 0; i < 10; i++) 
         printf("%d | ", pares[i]);
-    }
     printf("\n");
+    
     return 0;
 }
