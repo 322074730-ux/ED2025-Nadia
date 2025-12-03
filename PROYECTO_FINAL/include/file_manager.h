a3e6352ccfbebe315165b5c65250e5d1f8ba407c
Olvera García Nadia <322074730@pcpuma.acatlan.unam.mx>
2025-12-03 13:28:54 -0600

    Add file management functions to file_manager.h

--- PROYECTO_FINAL/include/file_manager.h
+++ PROYECTO_FINAL/include/file_manager.h
@@ -1,1 +1,11 @@
+#ifndef FILE_MANAGER_H
+#define FILE_MANAGER_H
 
+// Funciones de manejo de archivos
+void guardarOperacion(const char* expresion, const char* resultado, const char* tipo);
+void leerHistorial();
+void limpiarHistorial();
+void guardarResultadoEvaluacion(const char* expresion, double resultado);
+void mostrarEstadisticas();
+
+#endif
