b50b4d2d23751251b8c5ceb7817015cb4a1e62d8
Olvera García Nadia <322074730@pcpuma.acatlan.unam.mx>
2025-12-03 13:27:42 -0600

    Add header file for simple linked list implementation

--- PROYECTO_FINAL/include/list.h
+++ PROYECTO_FINAL/include/list.h
@@ -1,1 +1,24 @@
+#ifndef LIST_H
+#define LIST_H
 
+// Nodo de lista simple
+typedef struct _NodoSimple {
+    char* valor;
+    struct _NodoSimple* proximo;
+} NodoSimple;
+
+// Lista simple
+typedef struct {
+    NodoSimple* inicio;
+    NodoSimple* fin;
+    int total;
+} ListaSimple;
+
+// Funciones de lista simple
+ListaSimple* crearListaSimple();
+void liberarListaSimple(ListaSimple* lista);
+void agregarListaSimple(ListaSimple* lista, const char* valor);
+void imprimirListaSimple(ListaSimple* lista);
+int contarListaSimple(ListaSimple* lista);
+
+#endif
