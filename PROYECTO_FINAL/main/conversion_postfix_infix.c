d830132418608bd548f8004c54bdd8ec1230d1f9
Olvera García Nadia <322074730@pcpuma.acatlan.unam.mx>
2025-12-03 13:33:23 -0600

    Add function to convert postfix to infix expression
    Implement conversion from postfix to infix expression, handling operators and operands with error checking for invalid expressions and memory allocation.

--- PROYECTO_FINAL/main/conversion_postfix_infix.c
+++ PROYECTO_FINAL/main/conversion_postfix_infix.c
@@ -1,1 +1,65 @@
+#include "conversion_postfix_infix.h"
+#include "stack.h"
+#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+// Convertir postfija a infija
+char* convertirPostfijaAInfija(const char* postfija) {
+    if (!postfija || strlen(postfija) == 0) {
+        return strdup("");
+    }
+    
+    StringStack* pila = createStringStack();
+    
+    for (int i = 0; postfija[i] != '\0'; i++) {
+        // Saltar espacios
+        while (postfija[i] == ' ') i++;
+        if (postfija[i] == '\0') break;
+        
+        char token[2] = {postfija[i], '\0'};
+        
+        // Si es operando
+        if (isalnum(postfija[i])) {
+            stringStackPush(pila, token);
+        }
+        // Si es operador
+        else if (esCaracterOperador(postfija[i])) {
+            if (stringStackSize(pila) < 2) {
+                destroyStringStack(pila);
+                return strdup("ERROR: Expresión postfija inválida");
+            }
+            
+            char* operando2 = stringStackPop(pila);
+            char* operando1 = stringStackPop(pila);
+            
+            // Crear expresión infija con paréntesis
+            char* expresion = (char*)malloc(strlen(operando1) + strlen(operando2) + 4);
+            if (!expresion) {
+                free(operando1);
+                free(operando2);
+                destroyStringStack(pila);
+                return strdup("ERROR: Memoria insuficiente");
+            }
+            
+            sprintf(expresion, "(%s %c %s)", operando1, postfija[i], operando2);
+            stringStackPush(pila, expresion);
+            
+            free(operando1);
+            free(operando2);
+            free(expresion);
+        }
+    }
+    
+    if (stringStackSize(pila) != 1) {
+        destroyStringStack(pila);
+        return strdup("ERROR: Expresión postfija inválida");
+    }
+    
+    char* resultado = strdup(stringStackPop(pila));
+    destroyStringStack(pila);
+    
+    return resultado;
+}
