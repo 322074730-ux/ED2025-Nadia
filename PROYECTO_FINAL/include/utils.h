f696d372dece346581ba0baba02ef7a0436470e8
Olvera García Nadia <322074730@pcpuma.acatlan.unam.mx>
2025-12-03 12:10:29 -0600

    Update utils.h

--- PROYECTO_FINAL/include/utils.h
+++ PROYECTO_FINAL/include/utils.h
@@ -1,1 +1,28 @@
+#ifndef UTILS_H
+#define UTILS_H
 
+#include <stdbool.h>
+#include <ctype.h>
+
+// Prototipos de funciones esenciales
+bool esOperador(char c);
+bool esOperando(char c);
+int precedencia(char operador);
+bool esParentesis(char c);
+bool esParentesisApertura(char c);
+bool esParentesisCierre(char c);
+bool caracteresValidos(const char* expresion);
+int contarTokens(const char* expresion);
+void trimEspacios(char* str);
+bool expresionBalanceada(const char* expresion);
+bool esNumero(const char* str);
+double convertirADouble(const char* str);
+void limpiarBuffer();
+int leerEntero(const char* mensaje);
+double leerDouble(const char* mensaje);
+void leerCadena(char* buffer, int tamaño, const char* mensaje);
+void mostrarError(const char* mensaje);
+void mostrarExito(const char* mensaje);
+void pausarPantalla();
+
+#endif
