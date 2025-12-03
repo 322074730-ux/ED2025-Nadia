95f0cbbaecdfddc7c43b812fc3b3fd1ac69fd6e8
Olvera García Nadia <322074730@pcpuma.acatlan.unam.mx>
2025-12-03 13:30:41 -0600

    Add header guards and function declarations
    Add header guards and function declarations for infix to postfix conversion.

--- PROYECTO_FINAL/include/conversion_infix_postfix.h
+++ PROYECTO_FINAL/include/conversion_infix_postfix.h
@@ -1,1 +1,7 @@
+#ifndef CONVERSION_INFIX_POSTFIX_H
+#define CONVERSION_INFIX_POSTFIX_H
 
+char* convertirInfijaAPostfija(const char* infija);
+char* convertirInfijaAPostfijaConPaso(const char* infija);
+
+#endif
