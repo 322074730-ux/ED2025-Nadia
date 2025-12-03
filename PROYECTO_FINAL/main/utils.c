03a65c9f16d25006611e6a5bd973bf78444c8834
Olvera García Nadia <322074730@pcpuma.acatlan.unam.mx>
2025-12-03 16:36:46 -0600

    Update utils.c

--- PROYECTO_FINAL/main/utils.c
+++ PROYECTO_FINAL/main/utils.c
@@ -1,23 +1,24 @@
-#include "utils.h"
+#include "include/utils.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <math.h>
 
-// Verificar si un caracter es operador
-bool esOperador(char c){
-    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+void limpiarBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
 }
 
-// Verificar si es un operando (letra o digito)
-bool esOperando(char c){
-    return (isalnum(c) != 0);
+int esNumero(char c) {
+    return (c >= '0' && c <= '9') || c == '.';
 }
 
-// Obtener precedencia de operador
-int precedencia(char operador){
-    switch (operador){
+int esCaracterOperador(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
+}
+
+int precedencia(char operador) {
+    switch (operador) {
         case '+':
         case '-':
             return 1;
@@ -31,172 +32,98 @@ int precedencia(char operador){
     }
 }
 
-// Verificar si es paréntesis
-bool esParentesis(char c){
-    return (c == '(' || c == ')');
+int esOperadorValido(char c) {
+    return esCaracterOperador(c);
 }
 
-// Verificar si es paréntesis de apertura
-bool esParentesisApertura(char c){
-    return (c == '(');
+int esParentesis(char c) {
+    return c == '(' || c == ')';
 }
 
-// Verificar si es paréntesis de cierre
-bool esParentesisCierre(char c) {
-    return (c == ')');
-}
-
-// Validar caracteres en la expresión
-bool caracteresValidos(const char* expresion){
-    for (int i=0; expresion[i] != '\0'; i++){
-        char c=expresion[i];
-        if (!esOperando(c) && !esOperador(c) && !esParentesis(c) && c != ' ' && c != '.'){
-            return false;
-        }
+void leerCadena(char* buffer, int tamano, const char* mensaje) {
+    if (mensaje != NULL) {
+        printf("%s", mensaje);
     }
-    return true;
-}
-
-// Contar tokens en la expresión
-int contarTokens(const char* expresion){
-    int count=0;
-    int i=0;
-    int len=strlen(expresion);
     
-    while (i<len){
-        // Saltar espacios
-        while (i<len && expresion[i] == ' '){
-            i++;
-        }
-        if (i<len){
-            count++;
-            
-            // Si es operando 
-            if (esOperando(expresion[i])){
-                while (i < len && (esOperando(expresion[i]) || expresion[i] == '.')){
-                    i++;
-                }
-            }
-            // Si es operador o parentesis
-            else if (esOperador(expresion[i]) || esParentesis(expresion[i])){
-                i++;
-            }
+    fgets(buffer, tamano, stdin);
+    
+    // Eliminar salto de línea
+    size_t len = strlen(buffer);
+    if (len > 0 && buffer[len - 1] == '\n') {
+        buffer[len - 1] = '\0';
+    }
+}
+
+void formatearExpresion(char* expresion) {
+    int i, j = 0;
+    char temp[1000];
+    
+    for (i = 0; expresion[i] != '\0'; i++) {
+        if (expresion[i] != ' ') {
+            temp[j++] = expresion[i];
         }
     }
+    temp[j] = '\0';
+    strcpy(expresion, temp);
+}
+
+int validarExpresion(const char* expresion) {
+    int parentesis = 0;
+    int longitud = strlen(expresion);
+    
+    if (longitud == 0) return 0;
+    
+    for (int i = 0; i < longitud; i++) {
+        char actual = expresion[i];
+        
+        if (actual == '(') {
+            parentesis++;
+        } else if (actual == ')') {
+            parentesis--;
+            if (parentesis < 0) return 0;
+        } else if (esCaracterOperador(actual)) {
+            if (i == 0 || i == longitud - 1) return 0;
+            if (esCaracterOperador(expresion[i-1]) || expresion[i-1] == '(') return 0;
+        }
+    }
+    
+    return parentesis == 0;
+}
+
+int contarOperandos(const char* expresion) {
+    int count = 0;
+    int longitud = strlen(expresion);
+    
+    for (int i = 0; i < longitud; i++) {
+        if (esNumero(expresion[i])) {
+            count++;
+            while (i < longitud && esNumero(expresion[i])) i++;
+            i--;
+        } else if (isalpha(expresion[i])) {
+            count++;
+        }
+    }
+    
     return count;
 }
 
-// Eliminar espacios al principio y final
-void trimEspacios(char* str){
-    int i=0, j=0;
+int contarOperadores(const char* expresion) {
+    int count = 0;
     
-    // Eliminar espacios al inicio
-    while (str[i]==' '){
-        i++;
-    }
-    // Mover caracteres
-    while (str[i] != '\0') {
-        if (str[i] != ' ' || (str[i] == ' ' && str[i+1] != ' ' && str[i+1] != '\0')){
-            str[j]=str[i];
-            j++;
+    for (int i = 0; expresion[i] != '\0'; i++) {
+        if (esCaracterOperador(expresion[i])) {
+            count++;
         }
-        i++;
     }
-    str[j] = '\0';
-    // Eliminar espacio final si existe
-    if (j>0 && str[j-1] == ' '){
-        str[j-1] = '\0';
-    }
-}
-
-// Verificar si la expresión tiene paréntesis balanceados
-bool expresionBalanceada(const char* expresion){
-    int balance = 0;
     
-    for (int i = 0; expresion[i] != '\0'; i++){
-        if (expresion[i] == '('){
-            balance++;
-        } else if (expresion[i] == ')'){
-            balance--;
-            if (balance<0){
-                return false;
-            }
-        }
-    }
-    return (balance == 0);
+    return count;
 }
 
-// Verificar si una cadena es un número válido
-bool esNumero(const char* str){
-    if (str == NULL || *str == '\0'){
-        return false;
-    }   
-    int puntoDecimal=0;
-    for (int i=0; str[i] != '\0'; i++){
-        if (i==0 && str[i] == '-') {
-            continue;
-        }
-        if (str[i] == '.') {
-            puntoDecimal++;
-            if (puntoDecimal>1) {
-                return false;
-            }
-        } else if (!isdigit(str[i])) {
-            return false;
-        }
-    }
-    return true;
+void limpiarPantalla() {
+    system("cls");
 }
 
-// Convertir cadena a double
-double convertirADouble(const char* str){
-    return atof(str);
-}
-
-// Limpiar buffer de entrada
-void limpiarBuffer(){
-    int c;
-    while ((c=getchar()) != '\n' && c != EOF);
-}
-
-// Leer un entero con mensaje
-int leerEntero(const char* mensaje) {
-    int valor;
-    printf("%s", mensaje);
-    scanf("%d", &valor);
-    limpiarBuffer();
-    return valor;
-}
-
-// Leer un double con mensaje
-double leerDouble(const char* mensaje) {
-    double valor;
-    printf("%s", mensaje);
-    scanf("%lf", &valor);
-    limpiarBuffer();
-    return valor;
-}
-
-void leerCadena(char* buffer, int tamaño, const char* mensaje) {
-    printf("%s", mensaje);
-    fgets(buffer, tamaño, stdin);
-    // Eliminar salto de línea
-    size_t len = strlen(buffer);
-    if (len>0 && buffer[len-1] == '\n') {
-        buffer[len-1] = '\0';
-    }
-}
-
-void mostrarError(const char* mensaje) {
-    printf("\n ERROR: %s\n", mensaje);
-}
-
-void mostrarExito(const char* mensaje) {
-    printf("\n %s\n", mensaje);
-}
-
-void pausarPantalla() {
-    printf("\nPresiona Enter para continuar...");
-    limpiarBuffer();
+void pausar() {
+    printf("\nPresione Enter para continuar...");
     getchar();
 }
