1e7a9351a5e11f3dc38ec340a964ec1d9796811f
Olvera García Nadia <322074730@pcpuma.acatlan.unam.mx>
2025-12-03 13:25:16 -0600

    Add doubly linked list structure and functions

--- PROYECTO_FINAL/include/dlist.h
+++ PROYECTO_FINAL/include/dlist.h
@@ -1,1 +1,29 @@
+#ifndef DLIST_H
+#define DLIST_H
 
+// Nodo de lista doblemente enlazada
+typedef struct _NodoLista {
+    char* dato;
+    struct _NodoLista* anterior;
+    struct _NodoLista* siguiente;
+} NodoLista;
+
+// Lista doblemente enlazada
+typedef struct {
+    NodoLista* cabeza;
+    NodoLista* cola;
+    int cantidad;
+} ListaDoble;
+
+// Funciones de lista doble
+ListaDoble* crearListaDoble();
+void destruirListaDoble(ListaDoble* lista);
+void agregarFinalLista(ListaDoble* lista, const char* dato);
+void agregarInicioLista(ListaDoble* lista, const char* dato);
+void mostrarLista(ListaDoble* lista);
+void mostrarListaReversa(ListaDoble* lista);
+int tamanoLista(ListaDoble* lista);
+char* obtenerElemento(ListaDoble* lista, int indice);
+void eliminarElemento(ListaDoble* lista, int indice);
+
+#endif
