3d7b6c4053886bd77709da9fd969c9c1a1cdbbf0
Olvera García Nadia <322074730@pcpuma.acatlan.unam.mx>
2025-12-03 23:09:58 -0600

    Update conversion_postfix_prefix.c

--- PROYECTO_FINAL/main/conversion_postfix_prefix.c
+++ PROYECTO_FINAL/main/conversion_postfix_prefix.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include "../Include/utils.h"
 
-// Declaraciones de funciones externas
 char* convertirPostfijaAInfija(const char* postfija);
 char* convertirInfijaAPrefija(const char* infija);
 
@@ -12,20 +11,17 @@ char* convertirPostfijaAPrefija(const char* postfija) {
         return strdup("");
     }
     
-    // Paso 1: Convertir postfija a infija
     char* infija = convertirPostfijaAInfija(postfija);
     if (!infija) {
         return strdup("ERROR: Conversion postfija a infija fallida");
     }
     
-    // Verificar si hay error
     if (strstr(infija, "ERROR")) {
         char* resultado = strdup(infija);
         free(infija);
         return resultado;
     }
     
-    // Paso 2: Convertir infija a prefija
     char* prefija = convertirInfijaAPrefija(infija);
     
     free(infija);
